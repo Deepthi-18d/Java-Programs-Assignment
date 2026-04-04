bc0ff239310266fed480c0c1370477389f79c541
Deepthi-18d <deepthirayee@gmail.com>
2026-04-04 23:15:42 +0530

    Add files via upload

---
+++ CountryCapitalList.java
@@ -0,0 +1,66 @@
+package basics;
+
+
+import java.util.HashMap;
+import java.util.List;
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class CountryCapitalList extends JFrame {
+
+    private JList<String> countryList;
+    private HashMap<String, String> capitalMap;
+
+    public CountryCapitalList() {
+
+        // Create map of countries and capitals
+        capitalMap = new HashMap<>();
+        capitalMap.put("USA", "Washington D.C.");
+        capitalMap.put("India", "New Delhi");
+        capitalMap.put("Vietnam", "Hanoi");
+        capitalMap.put("Canada", "Ottawa");
+        capitalMap.put("Denmark", "Copenhagen");
+        capitalMap.put("France", "Paris");
+        capitalMap.put("Great Britain", "London");
+        capitalMap.put("Japan", "Tokyo");
+        capitalMap.put("Africa", "Addis Ababa");
+        capitalMap.put("Greenland", "Nuuk");
+        capitalMap.put("Singapore", "Singapore");
+
+        // Create list model
+        DefaultListModel<String> model = new DefaultListModel<>();
+        for (String country : capitalMap.keySet()) {
+            model.addElement(country);
+        }
+
+        // Create JList
+        countryList = new JList<>(model);
+
+        // Add listener
+        countryList.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()) {
+
+                    List<String> selected = countryList.getSelectedValuesList();
+
+                    for (String country : selected) {
+                        System.out.println("Capital of " + country + " is: " + capitalMap.get(country));
+                    }
+                }
+            }
+        });
+
+        add(new JScrollPane(countryList));
+
+        setTitle("Country Capitals");
+        setSize(300, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new CountryCapitalList();
+    }
+}
