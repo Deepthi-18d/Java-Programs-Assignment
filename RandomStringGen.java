7df8d2d09e90ab326d48e8c4375c947adaf722e4
Deepthi-18d <deepthirayee@gmail.com>
2026-04-04 20:33:23 +0530

    Add files via upload

---
+++ RandomStringGen.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class RandomStringGen {
+	    public static String generateRandomString(int length) {
+	        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	        StringBuilder random = new StringBuilder();
+
+	        for (int i = 0; i < length; i++) {
+	            int index = (int) (Math.random() * characters.length());
+	            random.append(characters.charAt(index));
+	        }
+	        return random.toString();
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println(generateRandomString(10));
+	    }
+	}
+
