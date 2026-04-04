1ba381e888e3db7f3c12a1342211765894a56f21
Deepthi-18d <deepthirayee@gmail.com>
2026-04-04 20:38:47 +0530

    Add files via upload

---
+++ CountWords.java
@@ -0,0 +1,13 @@
+package basics;
+
+	public class CountWords {
+	    public static int countWords(String str) {
+	        if (str == null || str.trim().isEmpty()) return 0;
+	        return str.trim().split("\\s+").length;
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println(countWords("Hello world from Java")); 
+	    }
+	}
+
