b2670e3a8c06f7ebb5aa91b7bdbf7686482d40be
LENOVO <LENOVO@LAPTOP-0921MJUT>
2026-04-19 13:11:39 +0530

    commit

---
+++ src/main/java/com/example/SessionTrackingServlet.java
@@ -0,0 +1,63 @@
+package com.example;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/SessionTracker")
+public class SessionTrackingServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Get session object
+        HttpSession session = request.getSession(true);
+
+        // Session details
+        Date createTime = new Date(session.getCreationTime());
+        Date lastAccessTime = new Date(session.getLastAccessedTime());
+        String sessionId = session.getId();
+
+        // Visit count
+        Integer visitCount = (Integer) session.getAttribute("visitCount");
+
+        if (visitCount == null) {
+            visitCount = 1;
+        } else {
+            visitCount++;
+        }
+
+        // Store updated count
+        session.setAttribute("visitCount", visitCount);
+
+        // Set session expiry (1 minute)
+        session.setMaxInactiveInterval(60);
+
+        // Set response type
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        // HTML Output
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head><title>Session Tracking Demo</title></head>");
+        out.println("<body>");
+
+        out.println("<h2>Session Tracking Information</h2>");
+        out.println("<p><b>Session ID:</b> " + sessionId + "</p>");
+        out.println("<p><b>Session Creation Time:</b> " + createTime + "</p>");
+        out.println("<p><b>Last Access Time:</b> " + lastAccessTime + "</p>");
+        out.println("<p><b>Visit Count:</b> " + visitCount + "</p>");
+
+        out.println("<p style='color:red;'>Session expires in 1 minute</p>");
+
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
