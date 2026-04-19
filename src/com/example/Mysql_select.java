fc4a96ca64b0c2523a9a1ac29f0f3fc2537211f7
LENOVO <LENOVO@LAPTOP-0921MJUT>
2026-04-19 20:01:31 +0530

    commit

---
+++ src/com/example/Mysql_select.java
@@ -0,0 +1,60 @@
+package com.example;
+
+
+	import java.sql.*;
+	import java.util.Properties;
+
+	public class Mysql_select {
+	    public static void main(String[] args) {
+	        try {
+	            Connection dbConnection = null;
+
+	            try {
+	                String url = "jdbc:mysql://localhost/test";
+	                Properties info = new Properties();
+	                info.put("user", "root");
+	                info.put("password", "Dhhh@63788182");
+
+	                dbConnection = DriverManager.getConnection(url, info);
+
+	                if (dbConnection != null) {
+	                    System.out.println("Successfully connected to MySQL database test");
+	                }
+	            } catch (SQLException ex) {
+	                System.out.println("An error occurred while connecting MySQL database");
+	                ex.printStackTrace();
+	            }
+
+	            // SELECT query
+	            String query = "SELECT * FROM coffee";
+	            Statement st = dbConnection.createStatement();
+	            ResultSet rs = st.executeQuery(query);
+
+	            while (rs.next()) {
+	                int id = rs.getInt("id");
+	                String coffee_name = rs.getString("coffee_name");
+	                int price = rs.getInt("price");
+
+	                System.out.format("\n%d, %s, %d", id, coffee_name, price);
+	            }
+	            rs.close();
+
+	            // INSERT using PreparedStatement (correct way)
+	            String insertQuery = "INSERT INTO coffee (coffee_name, price) VALUES (?, ?)";
+	            PreparedStatement stmt = dbConnection.prepareStatement(insertQuery);
+
+	            stmt.setString(1, "Tajmahal");
+	            stmt.setInt(2, 950);
+
+	            stmt.executeUpdate();
+	            stmt.close();
+
+	            System.out.println("\nRecord inserted successfully!");
+
+	        } catch (Exception e) {
+	            System.err.println("Got an exception!");
+	            System.err.println(e.getMessage());
+	        }
+	    }
+	}
+
