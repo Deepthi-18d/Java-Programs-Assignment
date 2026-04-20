1972bfab586dad1f9496b87063773ae88c60afbb
Deepthi-18d <deepthirayee@gmail.com>
2026-04-20 22:01:48 +0530

    Update Coffee_Delete.java

--- src/com/exampe/Coffee_Delete.java
+++ src/com/exampe/Coffee_Delete.java
@@ -1,3 +1,5 @@
+//Read all the existing records from the table coffee which is from the database test and delete an
+//existing coffee product from the table with its id.
 package com.exampe;
 import java.sql.*;
 import java.util.Properties;
