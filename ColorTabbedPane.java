715be3f6b9b84d8fc5317e031dc1857279fd884d
Deepthi-18d <deepthirayee@gmail.com>
2026-04-20 21:37:15 +0530

    Update ColorTabbedPane.java

--- ColorTabbedPane.java
+++ ColorTabbedPane.java
@@ -1,6 +1,6 @@
+//Develop a Swing program in Java to create a Tabbed Pan of Cyan, Magenta and Yellow and
+//display the concerned color whenever the specific tab is selected in the Pan
 package basics;
-
-
 import java.awt.Color;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
